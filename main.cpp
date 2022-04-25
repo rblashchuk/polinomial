a66b13f52336f9bd0c56fd64ebb2b6f22f507cdf
Rblashchuk <rblashchuk@gmail.com>
2022-04-26 01:12:28 +0300

    pow added, +check

--- main.cpp
+++ main.cpp
@@ -5,46 +5,74 @@
 
 using namespace std;
 
+constexpr long long pow(int x, int p) {
+    if (p == 0) {
+        return 1;
+
+    } else if (p == 1) {
+        return x;
+
+    } else if (p % 2 == 1) {
+        return pow(x * x, p / 2) * x;
+
+    } else {
+        return pow(x * x, p / 2);
+    }
+}
+
 struct monomial {
     int degree;
     int coef;
 };
 
-
 template<int len>
 class Polynomial {
 public:
-    constexpr Polynomial(initializer_list<monomial> &monomials) {
-        int count = 0;
-        for (auto &i: monomials) {
-            poly[count++] = i;
+    constexpr Polynomial(initializer_list<monomial> monomials) {
+        int ind = 0;
+        for (auto &elem: monomials) {
+            poly[ind] = elem;
+            ind++;
         }
     }
 
-    constexpr int val_at_point(const int x_value) {
-        int result(0);
-        for (auto &i: poly) {
-            result += pow(x_value, i.degree) * i.coef;
+    constexpr int val_at_point(const int x_value) const{
+        int ans(0);
+        for (auto &elem: poly) {
+            ans += pow(x_value, elem.degree) * elem.coef;
         }
-        return result;
+        return ans;
     }
 
-    friend ostream &operator<<(ostream &out, const Polynomial &polyn) {
+    friend ostream &operator<<(ostream &out, const Polynomial &polynomial) {
         for (int i = 0; i < len; i++) {
-            char sign = '+';
-            if (polyn.poly[i].coef < 0) {
-                sign = '-';
+            char s = '+';
+            if (polynomial.poly[i].coef < 0) {
+                s = '-';
             }
-            out << sign << ' ' <<  polyn.poly[i].coef << "x^" << polyn.poly[i].degree << " ";
+
+            if(polynomial.poly[i].coef == 1) {
+                if (i || s == '-')
+                    out << s << ' ' << "x^" << polynomial.poly[i].degree << " ";
+                else out << "x^" << polynomial.poly[i].degree << " ";
+            }
+
+            else {
+                if (i || s == '-')
+                    out << s << ' ' << polynomial.poly[i].coef << "x^" << polynomial.poly[i].degree << " ";
+                else out << polynomial.poly[i].coef << "x^" << polynomial.poly[i].degree << " ";
+            }
+
+
         }
         return out;
     }
 protected:
-    monomial poly[len];
+    monomial poly[len]{};
 };
 
-template <int n>
-struct Test{
+template <int n> //check
+struct A{
     int val = n;
 };
 
@@ -55,8 +83,13 @@ TEST(VALUE_TEST, TEST){
     ASSERT_TRUE(res == 10);
 };
 
-
 int main() {
+    constexpr Polynomial<4> pol = {monomial{2, 5}, monomial{1, 3}, monomial{3, 1}, monomial{4, 1}};
+    constexpr int res = pol.val_at_point(1);
+    cout << pol;
+
+    A<res> test;
+
     RUN_ALL_TESTS();
     return 0;
 }
